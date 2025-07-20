a0f47de2cdc50eae259000eada86664587f7833e
Rui Martins <ruimartinsptl@users.noreply.github.com>
2025-07-20 14:51:40 +0200

    fix: Fix URL to Youtube inside the context_processors.py

--- pycon_portugal_2024/site/context_processors.py
+++ pycon_portugal_2024/site/context_processors.py
@@ -54,7 +54,7 @@ def links(request):
         "social_media": {
             "x": "https://x.com/PyConPT",
             "slack": "https://join.slack.com/t/pyconportugal/shared_invite/zt-1ckszg1ye-QDgxx3lOkC15Ocal8xhCSg",
-            "youtube": "https://www.youtube.com/@DisjoinedTech94",
+            "youtube": "https://www.youtube.com/@pyconportugal",
             "linkedin": "https://www.linkedin.com/company/84778227",
             "github": "https://github.com/pyconpt/",
         },
